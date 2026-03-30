28e2d3a53e7e086abeaf63883bbe4dae12b0aab0
Mike Davidson <mike.davidson@omnibus.uk.com>
2026-03-30 14:34:44 +0100

    fixed build issues, added style loaders

--- apps/navtor-test-app/webpack.config.js
+++ apps/navtor-test-app/webpack.config.js
@@ -1,20 +1,36 @@
-const path = require("path");
+const { join } = require('path');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 
 module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-}
+  entry: './src/main.ts',
+  output: {
+    path: join(__dirname, 'dist/apps/navtor-test-app'),
+    filename: 'js/[name].[contenthash].js',
+    chunkFilename: 'js/[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      index: './src/index.html',
+      styles: ['./src/styles.scss'],
+      outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
+      optimization: process.env['NODE_ENV'] === 'production',
+    }),
+  ],
+};
